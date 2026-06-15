6a0ab420ae142805526974382bbf18c2d89257a6
irfanzzs-log <syah90009@gmail.com>
2026-06-15 16:36:17 +0700

    update v0.6

--- src/app/[theme]/[slug]/page.jsx
+++ src/app/[theme]/[slug]/page.jsx
@@ -1,6 +1,44 @@
 import { themes } from "@/lib/db";
 import Light from "@/component/theme/light";
 import { notFound } from "next/navigation";
+import {supabase} from "@/lib/clientConnection";
+
+export async function generateMetadata({ params }) {
+  const { theme, slug } = await params;
+  const { data, error } = await supabase
+      .from('wedding')
+      .select(`*`)
+      .eq('slug', slug)
+      .single();
+
+  if (error) {
+      console.error('Error fetching data:', error);
+      return {
+        title: "Undangan Pernikahan",
+        description: "Undangan pernikahan digital yang elegan dan mudah digunakan.",
+      };
+  }
+  
+
+  return {
+    title: `${slug} | Wedding Invitation`,
+
+    description: data?.description,
+
+    openGraph: {
+      title: `${slug} | Wedding Invitation`,
+      description: data?.story,
+      images: [
+        {
+          url: `/${theme}/${slug}/2.webp`,
+          width: 1200,
+          height: 630,
+        },
+      ],
+    },
+
+  };
+}
 
 export default async function Page({ params }) {
  const { theme, slug } = await params;
